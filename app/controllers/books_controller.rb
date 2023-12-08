8607bfdfd6a3730c68d8bffffa883a65478fc1b4
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2023-12-08 01:55:54 +0000

    2023/12/08/11/00

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,13 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
